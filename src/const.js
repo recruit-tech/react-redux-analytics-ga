c8b3ec37c1ec023bf359ce2d6325f516b116b8b5
横山　翔 <yuzutas0@users.noreply.github.com>
2018-11-26 14:41:56 +0900

    fix config params

--- src/const.js
+++ src/const.js
@@ -1,6 +1,7 @@
-export const configKeyTd = 'td'
-const namespace = 'analytics-td'
+export const configKeyTd = 'ga'
+const namespace = 'analytics-ga'
 export const debugNamespace = namespace+':debug'
 export const errorNamespace = namespace+':error'
 export const TYPE_EVENT = 'event'
 export const TYPE_PAGEVIEW = 'pageview'
+
